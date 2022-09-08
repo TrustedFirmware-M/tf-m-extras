e520d89bf2f1eae99a18e5fd33214ce26b96f105
Maulik  Patel <Maulik.Patel@arm.com>
2022-09-08 15:12:32 +0100

    Measured boot: Add test suite for Measured boot Service
    Signed-off-by: Maulik Patel <maulik.patel@arm.com>
    Change-Id: I11e0a024615ddf6193afe2decca6a3d6a31f4889

---
+++ partitions/measured_boot/test/non_secure/measured_boot_ns_interface_testsuite.c
@@ -0,0 +1,71 @@
+/*
+ * Copyright (c) 2022, Arm Limited. All rights reserved.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ *
+ */
+
+#include "test_framework_helpers.h"
+#include "extra_tests_common.h"
+#include "../measured_boot_tests_common.h"
+#include "../measured_boot_common.h"
+
+static struct test_t tfm_measured_boot_ns_tests[] = {
+    {&tfm_measured_boot_test_common_001, "TFM_NS_MEASURED_BOOT_TEST_1001",
+     "Extend and Read Measurement - valid input SHA256 measurement value"},
+    {&tfm_measured_boot_test_common_002, "TFM_NS_MEASURED_BOOT_TEST_1002",
+     "Extend and Read Measurement - valid input SHA512 measurement value"},
+    {&tfm_measured_boot_test_common_003, "TFM_NS_MEASURED_BOOT_TEST_1003",
+     "Extend and Read Measurement - invalid slot index"},
+    {&tfm_measured_boot_test_common_004, "TFM_NS_MEASURED_BOOT_TEST_1004",
+     "Extend Measurement - invalid measurement value size"},
+    {&tfm_measured_boot_test_common_005, "TFM_NS_MEASURED_BOOT_TEST_1005",
+     "Read Measurement - invalid input measurement buffer size"},
+    {&tfm_measured_boot_test_common_006, "TFM_NS_MEASURED_BOOT_TEST_1006",
+     "Extend Measurement - invalid signer id size"},
+    {&tfm_measured_boot_test_common_007, "TFM_NS_MEASURED_BOOT_TEST_1007",
+     "Read Measurement - invalid input signer id buffer size"},
+    {&tfm_measured_boot_test_common_008, "TFM_NS_MEASURED_BOOT_TEST_1008",
+     "Extend Measurement - invalid version size"},
+    {&tfm_measured_boot_test_common_009, "TFM_NS_MEASURED_BOOT_TEST_1009",
+     "Read Measurement - invalid input version buffer size"},
+    {&tfm_measured_boot_test_common_010, "TFM_NS_MEASURED_BOOT_TEST_1010",
+     "Extend Measurement - invalid input software type buffer size"},
+    {&tfm_measured_boot_test_common_011, "TFM_NS_MEASURED_BOOT_TEST_1011",
+     "Read Measurement - invalid software type size"},
+    {&tfm_measured_boot_test_common_012, "TFM_NS_MEASURED_BOOT_TEST_1012",
+     "Extend Measurement - NS unlock test"},
+    {&tfm_measured_boot_test_common_013, "TFM_NS_MEASURED_BOOT_TEST_1013",
+     "Extend Measurement - different signer id"}
+};
+
+static int32_t tfm_measured_boot_test(void)
+{
+    uint32_t i;
+    struct test_result_t result;
+
+    uint32_t test_count = (sizeof(tfm_measured_boot_ns_tests) /
+                           sizeof(tfm_measured_boot_ns_tests[0]));
+
+    TEST_LOG("  Executing Measured Boot Non-Secure tests \r\n");
+    for (i = 0; i < test_count; i++) {
+        TEST_LOG("  \r\n%s: %s \r\n", tfm_measured_boot_ns_tests[i].name,
+                                      tfm_measured_boot_ns_tests[i].desc);
+        tfm_measured_boot_ns_tests[i].test(&result);
+        if (result.val != TEST_PASSED) {
+            return EXTRA_TEST_FAILED;
+        }
+    }
+    return EXTRA_TEST_SUCCESS;
+}
+
+/* Define test suite for measured boot service tests */
+const struct extra_tests_t measured_boot_ns_t = {
+    .test_entry = tfm_measured_boot_test,
+    .expected_ret = EXTRA_TEST_SUCCESS,
+};
+
+int32_t extra_tests_init(struct extra_tests_t *internal_test_t)
+{
+    return register_extra_tests(internal_test_t, &measured_boot_ns_t);
+}
